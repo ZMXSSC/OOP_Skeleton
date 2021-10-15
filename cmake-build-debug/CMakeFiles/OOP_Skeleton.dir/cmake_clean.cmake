3eb1b2539d2a3e5afa235000cec4d7458f6d0789
ZMXSSC <alonzochu4@gmail.com>
2021-10-15 13:05:31 -0700

    initial commit

---
+++ cmake-build-debug/CMakeFiles/OOP_Skeleton.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OOP_Skeleton.dir/main.cpp.o"
+  "OOP_Skeleton"
+  "OOP_Skeleton.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OOP_Skeleton.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+class UCSD{
+    //TODO: Create an Abstract class(interface),
+    // where provides a function to check if a student is legal to drink.　
+};
+
+class Department{
+    //TODO: Create a “Department” class.
+    // Each object should have attributes of
+    // student’s name, student’s college and student’s age.
+
+
+    //TODO: Inside Department class,
+    // you want to create a function call
+    // introduce yourself” , “study” , setter, getter and constructor.
+};
+
+class ECE{
+    //TODO: or ECE class, provide an attribute to store student’s depth.
+    // Also, provide a unique function call “selection” which output what depth student is pursuing.
+    // In addition, you want to override “study” function
+    // using polymorphism technique because you know ECE student study really hard:).
+    // Don’t forget about your constructor.
+
+};
+
+class CSE{
+    //TODO: For CSE class, provide an attribute to store student’s favorite programming language.
+    // Also, provide a unique function call “fixBug” which output what favorite language a student is using to fix bugs.
+    // In addition, you want to override “study” function
+    // using polymorphism technique because you know CSE student study (sort of) hard:).
+    // Don’t forget about your constructor!
+
+};
+
+int main() {
+//    std::cout << "Hello, World!" << std::endl;
+    //TODO: Play around with it!
+    return 0;
+}
